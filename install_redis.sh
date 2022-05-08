bf6a96d862922d013eb00f5732b2a0279acbbe2c
root <root@localhost.localdomain>
2022-05-08 09:35:00 -0400

    Fixed redis to be accessible from remote machines

--- install_redis.sh
+++ install_redis.sh
@@ -6,5 +6,8 @@ sudo yum-config-manager --enable remi -y
 sudo yum install redis -y
 
 sed -i 's/protected-mode yes/protected-mode no/g' /etc/redis/redis.conf
-sudo systemctl start redis
+sed -i 's/^bind 127.0.0.1 -::1/bind * -::*/g' /etc/redis/redis.conf
+sudo firewall-cmd --zone=public --permanent --add-port=6379/tcp
+sudo firewall-cmd --reload
+sudo systemctl restart redis
 sudo systemctl enable redis
