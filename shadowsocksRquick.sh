8322c4a869303c8f7b4838e58661b1d3c66c700e
Maisam <maisam.vahidsafa@gmail.com>
2023-02-04 20:36:03 +0330

    Add c compiler install in shadowsocksRquick.sh

--- shadowsocksRquick.sh
+++ shadowsocksRquick.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+sudo apt-get install build-essential -y
 #=================================================================#
 #   System Required:  CentOS 6,7, Debian, Ubuntu                  #
 #   Description: One click Install ShadowsocksR Server            #
